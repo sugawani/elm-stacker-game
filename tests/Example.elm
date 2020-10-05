341c2526012f6d9a878cdeee43dbe1ad581cde76
sugawani <nekorollykk@gmail.com>
2020-10-06 00:41:16 +0900

    add getLightColor UnitTest

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,10 +2,29 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Main exposing (..)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    test "1+1は2"
-        (\_ -> Expect.equal 2 (1 + 1))
+    describe "test"
+        [ describe "getLightColor"
+            [ test "LightOnを渡すとredが返る"
+                (\_ ->
+                    let
+                        colorString =
+                            getLightColor LightOn
+                    in
+                    Expect.equal "red" colorString
+                )
+            , test "LightOffを渡すとwhiteが返る"
+                (\_ ->
+                    let
+                        colorString =
+                            getLightColor LightOff
+                    in
+                    Expect.equal "white" colorString
+                )
+            ]
+        ]
